8b383a98c20a0331281b32053812176f2edfc7f7
z5n0w <836702701zs@gmail.com>
2017-09-05 16:44:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 if [ ! -d ~/.vim ]; then
     mkdir -p ~/.vim/bundle
     cp ./vimrc ~/.vim/vimrc
-    cp -r ./Vundle.Vim ~/.vim/bundle/
+    cp -r ./Vundle.Vim ~/.vim/bundle/Vundle.vim
 else if [ -f ~/.vim/vimrc ]; then
         mv ~/.vim/vimrc ~/.vim/vimrc_bak
         echo "You old vimrc renameed as vimrc_bak"
