735e248cf285a27fda78df65fa4bc8af4bffc84d
z5n0w <836702701zs@gmail.com>
2018-02-10 23:52:16 +0800

    new version install scripts

--- install.sh
+++ install.sh
@@ -1,30 +1,51 @@
 #/bin/bash
 
-#check Vundle
-if [ ! -d ~/.vim ]; then
-    mkdir -p ~/.vim/bundle
-fi
+while getopts "avh" arg
+do 
+    case $arg in 
+        a)
+            # screen tmux zsh
+            FLIST=".screenrc .tmux.conf .zshrc"
+            echo "also apply $FLIST"
+            for i in $FLIST; do
+            	if [ -f ~/${i} ]; then
+            	    rm ~/${i}
+            	fi
+            	ln -s ${PWD}/${i} ~/${i}
+            done
+            ;&
+        v)
+            # vim 
+            # check Vundle
+            if [ ! -d ~/.vim ]; then
+                mkdir -p ~/.vim/bundle
+            fi
 
-if [ ! -d ~/.vim/bundle ]; then
-    mkdir ~/.vim/bundle
-fi
+            if [ ! -d ~/.vim/bundle ]; then
+                mkdir ~/.vim/bundle
+            fi
 
-if [ ! -d ~/.vim/bundle/Vundle.Vim ]; then
-    cp -r ${PWD}/Vundle.Vim ~/.vim/bundle/
-fi
+            if [ ! -d ~/.vim/bundle/Vundle.Vim ]; then
+                git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.Vim
+            else
+                echo "Vundle exist"
+            fi
 
-#replace vimrc
-if [ -f ~/.vim/vimrc ]; then
-    mv ~/.vim/vimrc ~/.vim/vimrc_bak
-    echo "You old vimrc renamed as _bak"
-fi
-ln -s ${PWD}/vimrc ~/.vim/vimrc
-
-#screen & tmux
-FLIST=".screenrc .tmux.conf"
-for i in $FLIST; do
-	if [ -f ~/${i} ]; then
-	    rm ~/${i}
-	fi
-	ln -s ${PWD}/${i} ~/${i}
+            # replace vimrc
+            if [ -f ~/.vim/vimrc ]; then
+                mv ~/.vim/vimrc ~/.vim/vimrc_bak
+                echo "old vimrc renamed as _bak"
+            fi
+            ln -s ${PWD}/vimrc ~/.vim/vimrc
+            ;;
+        h)
+            echo "./install.sh -v to apply vim config ONLY"
+            echo "./install.sh -a to apply all config "
+            ;;
+        ?)
+            echo "error arg"
+            ;;
+    esac
 done
+
+echo "./install.sh -h show uaage"
