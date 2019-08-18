7fb340764c2137ead9047ad007a2de5b6a066826
z5n0w <s@z5n0w-debian.z5n0w>
2019-08-18 10:03:20 +0800

    update install sh

--- install.sh
+++ install.sh
@@ -1,5 +1,24 @@
 #/bin/bash
 
+setVim(){
+	# using vim-plug as plugin manager
+	for i in "bundle" "autoload";do
+		if [ ! -d ~/.vim/${i} ]; then 
+		    mkdir -p ~/.vim/${i} 
+		fi
+	done
+	if [ -d ~/.vim/bundle/vim-plug ]; then 
+		rm -r ~/.vim/bundle/vim-plug 
+	fi 
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	# replace ori vimrc 
+	if [ -f ~/.vim/vimrc ]; then
+		mv ~/.vim/vimrc ~/.vim/vimrc_$(data)
+		echo "old vimrc renamed as _bak"
+	fi
+	ln -s ${PWD}/vimrc ~/.vim/vimrc
+}
+
 while getopts "av" arg
 do 
     case $arg in 
@@ -13,25 +32,10 @@ do
             	fi
             	ln -s ${PWD}/${i} ~/${i}
             done
+	    setVim
             ;;
         v)
-            # vim 
-            # using vim-plug as plugin manager
-            for i in "bundle" "autoload";do
-                if [ ! -d ~/.vim/${i} ]; then 
-                    mkdir -p ~/.vim/${i} 
-                fi
-            done
-            if [ -d ~/.vim/bundle/vim-plug ]; then 
-                rm -r ~/.vim/bundle/vim-plug 
-            fi 
-	    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-            # replace ori vimrc 
-            if [ -f ~/.vim/vimrc ]; then
-		mv ~/.vim/vimrc ~/.vim/vimrc_$(data)
-                echo "old vimrc renamed as _bak"
-            fi
-            ln -s ${PWD}/vimrc ~/.vim/vimrc
+	    setVim
             ;;
         ?)
             echo "./install.sh -v to apply vim config ONLY"
